710309fbc7c746c7df64a042759ca73da3acf54c
davejbax <davejbax@gmail.com>
2023-12-06 23:32:34 +0000

    Add day 3 (WIP: Terraform currently takes too long)

---
+++ day3/cell/main.tf
@@ -0,0 +1,53 @@
+variable "left" {
+  type        = string
+  description = "Letter to the left"
+}
+
+variable "text" {
+  type        = string
+  description = "All text from the cell rightwards"
+}
+
+variable "grid" {
+  type        = list(string)
+  description = "The entire grid"
+}
+
+variable "x" {
+  type        = number
+  description = "X coordinate"
+}
+
+variable "y" {
+  type        = number
+  description = "Y coordinate"
+}
+
+locals {
+  numbers = regexall("^\\d+", var.text)
+
+  is_valid = var.left == "." && length(local.numbers) > 0
+
+  adjacent = local.is_valid ? [
+    # Generates a set of adjacent coordinates, from (-1,-1) to (w+1,1)
+    for i in setproduct(range(-1, length(local.numbers[0]) + 2), range(-1, 2)) :
+    (
+      # To make things simpler, ignore ourselves
+      i[1] == 0 && i[0] >= 0 && i[0] < length(local.numbers[0])
+      ? "."
+      : try(substr(var.grid[var.y + i[1]], var.x + i[0], 1), ".")
+    )
+  ] : null
+
+  adjacent_to_symbol = local.is_valid ? anytrue([
+    for char in local.adjacent : (length(regexall("[^\\.0-9]", char)) > 0)
+  ]) : null
+}
+
+output "number" {
+  value = local.is_valid ? local.numbers[0] : null
+}
+
+output "adjacent_to_symbol" {
+  value = local.is_valid ? local.adjacent_to_symbol : false
+}
---
+++ day3/main.tf
@@ -0,0 +1,28 @@
+locals {
+
+  input       = file("input.txt")
+  input_lines = split("\n", trimspace(local.input))
+
+  grid_height = length(local.input_lines)
+  grid_width  = length(local.input_lines[0])
+
+  # N.B. range() has a limit of 1024, so range(w*h) doesn't work
+  grid_cell_coords = [for v in setproduct(range(local.grid_width), range(local.grid_height)) : "${v[0]}x${v[1]}"]
+
+}
+
+module "cell" {
+  for_each = toset(local.grid_cell_coords)
+  source   = "./cell"
+
+  left = try(substr(local.input_lines[split("x", each.key)[1]], split("x", each.key)[0] - 1, 1), ".")
+  text = try(substr(local.input_lines[split("x", each.key)[1]], split("x", each.key)[0], -1), ".")
+  grid = local.input_lines
+
+  x = split("x", each.key)[0]
+  y = split("x", each.key)[1]
+}
+
+output "part1" {
+  value = sum([for cell in module.cell : cell.number if cell.adjacent_to_symbol])
+}
